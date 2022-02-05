a2d98d34405470dfec667a319c5dee1d40c80a07
DESKTOP-T6RT55Q\ryanw <rwy5224@nyu.edu>
2022-02-05 17:16:34 -0500

    adding text

---
+++ Assets/Week 2/SimpleTextUpdate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SimpleTextUpdate : MonoBehaviour
+{
+
+    public TMP_Text text;
+    public string newText;
+    [SerializeField]
+    string otherText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = newText;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = otherText;
+        }
+    }
+}
